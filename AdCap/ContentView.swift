d41bdc6c36c4837f5f6781c754ecd21b2b676acb
Jeffrey Chen <j.chen14641@gmail.com>
2020-05-23 10:48:53 -0700

    Initial Commit

---
+++ AdCap/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  AdCap
+//
+//  Created by Jeffrey Chen on 5/23/20.
+//  Copyright © 2020 Jeffrey Chen. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
